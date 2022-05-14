bceefc5729b66944b4b460233b1be4f769173bd9
Phillip Henry <PhillHenry@gmail.com>
2022-05-14 10:06:50 +0100

    More refactoring.

--- src/main/scala/uk/co/odinconsultants/mymicroservices/MymicroservicesServer.scala
+++ src/main/scala/uk/co/odinconsultants/mymicroservices/MymicroservicesServer.scala
@@ -1,41 +1,41 @@
 package uk.co.odinconsultants.mymicroservices
 
 import cats.effect.{Async, Resource}
-import cats.syntax.all._
-import com.comcast.ip4s._
+import cats.syntax.all.*
+import com.comcast.ip4s.*
 import fs2.Stream
 import org.http4s.ember.client.EmberClientBuilder
 import org.http4s.ember.server.EmberServerBuilder
-import org.http4s.implicits._
+import org.http4s.implicits.*
 import org.http4s.server.middleware.Logger
 
 object MymicroservicesServer:
 
-  def stream[F[_]: Async]: Stream[F, Nothing] = {
+  def stream[F[_] : Async]: Stream[F, Nothing] = {
     for {
-      client <- Stream.resource(EmberClientBuilder.default[F].build)
+      client        <- Stream.resource(EmberClientBuilder.default[F].build)
       helloWorldAlg = HelloWorld.impl[F]
-      jokeAlg = Jokes.impl[F](client)
+      jokeAlg       = Jokes.impl[F](client)
 
       // Combine Service Routes into an HttpApp.
       // Can also be done via a Router if you
       // want to extract a segments not checked
       // in the underlying routes.
-      httpApp = (
+      httpApp       = (
         MymicroservicesRoutes.helloWorldRoutes[F](helloWorldAlg) <+>
-        MymicroservicesRoutes.jokeRoutes[F](jokeAlg)
-      ).orNotFound
+          MymicroservicesRoutes.jokeRoutes[F](jokeAlg)
+        ).orNotFound
 
       // With Middlewares in place
-      finalHttpApp = Logger.httpApp(true, true)(httpApp)
+      finalHttpApp  = Logger.httpApp(true, true)(httpApp)
 
-      exitCode <- Stream.resource(
+      exitCode      <- Stream.resource(
         EmberServerBuilder.default[F]
           .withHost(ipv4"0.0.0.0")
           .withPort(port"8082")
           .withHttpApp(finalHttpApp)
           .build >>
-        Resource.eval(Async[F].never)
+          Resource.eval(Async[F].never)
       )
     } yield exitCode
   }.drain
