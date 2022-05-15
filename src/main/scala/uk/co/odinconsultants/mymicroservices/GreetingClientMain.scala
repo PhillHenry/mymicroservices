13fbfb5e6e38ecd7d792017113bf20e9b3cd159a
Phillip Henry <PhillHenry@gmail.com>
2022-05-15 09:00:28 +0100

    Delay, not pure.

--- src/main/scala/uk/co/odinconsultants/mymicroservices/GreetingClientMain.scala
+++ src/main/scala/uk/co/odinconsultants/mymicroservices/GreetingClientMain.scala
@@ -1,6 +1,7 @@
 package uk.co.odinconsultants.mymicroservices
 
 import cats.effect.kernel.Async
+import cats.effect.kernel.Sync
 import cats.effect.{ExitCode, IO, IOApp}
 import fs2.Stream
 import org.http4s.ember.client.EmberClientBuilder
@@ -10,12 +11,19 @@ import cats.implicits._
 object GreetingClientMain extends IOApp.Simple:
   def run: IO[Unit] =
     makeCall[IO].as(ExitCode.Success)
+    
+  def printing[T](t: T): _ => T = _ => t
 
-  def makeCall[F[_]: Async]: F[Unit] =
+  def printAnReturn[F[_], T](t: T)(implicit F: Async[F]): F[T] = F.delay({
+    println(t)
+    t
+  })
+
+  def makeCall[F[_]: Async: Sync]: F[Unit] =
     val stream: Stream[F, Jokes.Joke] = for {
       client <- Stream.resource(EmberClientBuilder.default[F].build)
       jokeAlg: Jokes[F] = GreetingClient.impl[F](client)
-      joke   <- Stream.eval(jokeAlg.get.flatMap(x => {println(x); x}.pure)) // hmm, that's not pure...
+      joke   <- Stream.eval(jokeAlg.get.flatMap(printAnReturn))
     } yield {
       joke
     }
