456e1fa707783145fb02871c0373ae7edb199520
Phillip Henry <PhillHenry@gmail.com>
2022-05-29 10:31:57 +0100

    Another test.

--- src/test/scala/uk/co/odinconsultants/mymicroservices/GreetingsClientSpec.scala
+++ src/test/scala/uk/co/odinconsultants/mymicroservices/GreetingsClientSpec.scala
@@ -6,15 +6,26 @@ import org.http4s.implicits._
 import munit.CatsEffectSuite
 
 import uk.co.odinconsultants.mymicroservices.HttpHandler
+import uk.co.odinconsultants.mymicroservices.Jokes.JokeError
 
 class GreetingsClientSpec extends CatsEffectSuite {
 
   val Joke = Jokes.Joke("Boris Johnson")
+  val happyHandler = new HttpHandler[IO] {
+    def callHttp(req: Request[IO]): IO[Jokes.Joke] = IO.pure(Joke)
+  }
+
+  val Failure = new Throwable("error message")
+  val badHandler = new HttpHandler[IO] {
+      def callHttp(req: Request[IO]): IO[Jokes.Joke] = IO.raiseError(Failure)
+  }
 
   test("Happy path call") {
-    val handler = new HttpHandler[IO]{
-        def callHttp(req: Request[IO]): IO[Jokes.Joke] = IO.pure(Joke)
-    }
-    assertIO(GreetingClient.impl[IO](handler).get, Joke)
+    assertIO(GreetingClient.impl[IO](happyHandler).get, Joke)
+  }
+
+  test("Unhappy path call") {
+    val io = GreetingClient.impl[IO](badHandler).get
+    interceptIO[JokeError](io)
   }
 }
