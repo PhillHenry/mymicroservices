27cf67c6911088eebf01c89a157d52252d8da1ae
Phillip Henry <PhillHenry@gmail.com>
2022-05-29 10:01:16 +0100

    Refactor

--- src/main/scala/uk/co/odinconsultants/mymicroservices/GreetingClient.scala
+++ src/main/scala/uk/co/odinconsultants/mymicroservices/GreetingClient.scala
@@ -9,11 +9,25 @@ import org.http4s._
 import org.http4s.implicits._
 import cats.implicits.catsSyntaxMonadError
 
+trait HttpHandler[F[_]] {
+  def callHttp(req: Request[F]): F[Jokes.Joke]
+}
+
+class Http4sHandler[F[_]: Concurrent](C: Client[F], d: EntityDecoder[F, Jokes.Joke]) extends HttpHandler[F] {
+  def callHttp(req: Request[F]): F[Jokes.Joke] = {
+    C.expect[Joke](req)(d) 
+  }
+}
+
+object Http4sHandler{
+  def apply[F[_]: Concurrent](C: Client[F])(implicit d: EntityDecoder[F, Jokes.Joke]): Http4sHandler[F] = new Http4sHandler(C, d)
+}
+
 object GreetingClient {
-  def impl[F[_]: Concurrent](C: Client[F]): Jokes[F] = new Jokes[F]:
+  def impl[F[_]: Concurrent](handler: Http4sHandler[F]): Jokes[F] = new Jokes[F]:
     val dsl = new Http4sClientDsl[F]{}
     import dsl._
     def get: F[Jokes.Joke] =
-      C.expect[Joke](GET(uri"https://icanhazdadjoke.com/"))
+      handler.callHttp(GET(uri"https://icanhazdadjoke.com/"))
         .adaptError{ case t => JokeError(t)}
 }
--- src/main/scala/uk/co/odinconsultants/mymicroservices/GreetingClientMain.scala
+++ src/main/scala/uk/co/odinconsultants/mymicroservices/GreetingClientMain.scala
@@ -7,12 +7,15 @@ import fs2.Stream
 import org.http4s.ember.client.EmberClientBuilder
 import org.http4s.implicits._
 import cats.implicits._
+import org.http4s.client.Client
+import org.http4s.Request
+import org.http4s.EntityDecoder
 
 object GreetingClientMain extends IOApp.Simple:
   def run: IO[Unit] =
     makeCall[IO].as(ExitCode.Success)
 
-  def printAnReturn[F[_], T](t: T)(implicit F: Sync[F]): F[T] = F.delay({
+  def printReturning[F[_], T](t: T)(implicit F: Sync[F]): F[T] = F.delay({
     println(t)
     t
   })
@@ -20,8 +23,8 @@ object GreetingClientMain extends IOApp.Simple:
   def makeCall[F[_]: Async: Sync]: F[Unit] =
     val stream: Stream[F, Jokes.Joke] = for {
       client <- Stream.resource(EmberClientBuilder.default[F].build)
-      jokeAlg: Jokes[F] = GreetingClient.impl[F](client)
-      joke   <- Stream.eval(jokeAlg.get.flatMap(printAnReturn))
+      jokeAlg: Jokes[F] = GreetingClient.impl[F](Http4sHandler(client))
+      joke   <- Stream.eval(jokeAlg.get.flatMap(printReturning))
     } yield {
       joke
     }
